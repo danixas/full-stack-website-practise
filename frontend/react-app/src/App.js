c1c549a180bff8e0c3cae1f4b80e6e5f2a6b6ed1
Studijos <61288384+danixas@users.noreply.github.com>
2024-01-25 18:21:02 +0200

    initialized frontend with create react app, not connected to backend yet

---
+++ frontend/react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="button" type="button">Login</button>
+        <button className="button" type="button">Register</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
